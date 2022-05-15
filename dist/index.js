638ccd1157a2e4a267d5a67aaa242c4253e5db84
Phillip Hicks <phillip.hicks@immutable.com>
2022-05-15 15:26:34 +1000

    Add win32 docker cmd

--- dist/index.js
+++ dist/index.js
@@ -154,15 +154,27 @@ const path_1 = __importDefault(__nccwpck_require__(1017));
 const Docker = {
     run(image, parameters, silent = false) {
         return __awaiter(this, void 0, void 0, function* () {
-            const { actionFolder, editorVersion, workspace, projectPath, customParameters, testMode, coverageOptions, artifactsPath, useHostNetwork, sshAgent, gitPrivateToken, githubToken, runnerTemporaryPath, } = parameters;
-            const githubHome = path_1.default.join(runnerTemporaryPath, '_github_home');
-            if (!(0, fs_1.existsSync)(githubHome))
-                (0, fs_1.mkdirSync)(githubHome);
-            const githubWorkflow = path_1.default.join(runnerTemporaryPath, '_github_workflow');
-            if (!(0, fs_1.existsSync)(githubWorkflow))
-                (0, fs_1.mkdirSync)(githubWorkflow);
-            const testPlatforms = (testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]).join(';');
-            const command = `docker run \
+            let runCommand = '';
+            switch (process.platform) {
+                case 'linux':
+                    runCommand = this.getLinuxCommand(image, parameters);
+                    break;
+                case 'win32':
+                    runCommand = this.getWindowsCommand(image, parameters);
+            }
+            yield (0, exec_1.exec)(runCommand, undefined, { silent });
+        });
+    },
+    getWindowsCommand(image, parameters) {
+        const { actionFolder, editorVersion, workspace, projectPath, customParameters, testMode, coverageOptions, artifactsPath, useHostNetwork, sshAgent, gitPrivateToken, githubToken, runnerTemporaryPath, } = parameters;
+        const githubHome = path_1.default.join(runnerTemporaryPath, '_github_home');
+        if (!(0, fs_1.existsSync)(githubHome))
+            (0, fs_1.mkdirSync)(githubHome);
+        const githubWorkflow = path_1.default.join(runnerTemporaryPath, '_github_workflow');
+        if (!(0, fs_1.existsSync)(githubWorkflow))
+            (0, fs_1.mkdirSync)(githubWorkflow);
+        const testPlatforms = (testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]).join(';');
+        return `docker run \
         --workdir /github/workspace \
         --rm \
         --env UNITY_LICENSE \
@@ -177,6 +189,7 @@ const Docker = {
         --env COVERAGE_OPTIONS="${coverageOptions}" \
         --env COVERAGE_RESULTS_PATH="CodeCoverage" \
         --env ARTIFACTS_PATH="${artifactsPath}" \
+        --env GITHUB_WORKSPACE=c:/github/workspace \
         --env GITHUB_REF \
         --env GITHUB_SHA \
         --env GITHUB_REPOSITORY \
@@ -192,21 +205,73 @@ const Docker = {
         --env RUNNER_TOOL_CACHE \
         --env RUNNER_TEMP \
         --env RUNNER_WORKSPACE \
-        --env GIT_PRIVATE_TOKEN="${gitPrivateToken}" \
+        ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
+        ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
+        --volume "${workspace}":"c:/github/workspace" \
+        --volume "c:/regkeys":"c:/regkeys" \
+        --volume "C:/Program Files (x86)/Microsoft Visual Studio":"C:/Program Files (x86)/Microsoft Visual Studio" \
+        --volume "C:/Program Files (x86)/Windows Kits":"C:/Program Files (x86)/Windows Kits" \
+        --volume "C:/ProgramData/Microsoft/VisualStudio":"C:/ProgramData/Microsoft/VisualStudio" \
+        --volume "${actionFolder}/default-build-script":"c:/UnityBuilderAction" \
+        --volume "${actionFolder}/platforms/windows":"c:/steps" \
+        --volume "${actionFolder}/BlankProject":"c:/BlankProject" \
+        ${useHostNetwork ? '--net=host' : ''} \
+        ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
+        ${image} \
+        powershell c:/'steps/entrypoint.ps1`;
+    },
+    getLinuxCommand(image, parameters) {
+        const { actionFolder, editorVersion, workspace, projectPath, customParameters, testMode, coverageOptions, artifactsPath, useHostNetwork, sshAgent, gitPrivateToken, githubToken, runnerTemporaryPath, } = parameters;
+        const githubHome = path_1.default.join(runnerTemporaryPath, '_github_home');
+        if (!(0, fs_1.existsSync)(githubHome))
+            (0, fs_1.mkdirSync)(githubHome);
+        const githubWorkflow = path_1.default.join(runnerTemporaryPath, '_github_workflow');
+        if (!(0, fs_1.existsSync)(githubWorkflow))
+            (0, fs_1.mkdirSync)(githubWorkflow);
+        const testPlatforms = (testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]).join(';');
+        return `docker run \
+        --workdir /github/workspace \
+        --rm \
+        --env UNITY_LICENSE \
+        --env UNITY_LICENSE_FILE \
+        --env UNITY_EMAIL \
+        --env UNITY_PASSWORD \
+        --env UNITY_SERIAL \
+        --env UNITY_VERSION="${editorVersion}" \
+        --env PROJECT_PATH="${projectPath}" \
+        --env CUSTOM_PARAMETERS="${customParameters}" \
+        --env TEST_PLATFORMS="${testPlatforms}" \
+        --env COVERAGE_OPTIONS="${coverageOptions}" \
+        --env COVERAGE_RESULTS_PATH="CodeCoverage" \
+        --env ARTIFACTS_PATH="${artifactsPath}" \
+        --env GITHUB_WORKSPACE=/github/workspace \
+        --env GITHUB_REF \
+        --env GITHUB_SHA \
+        --env GITHUB_REPOSITORY \
+        --env GITHUB_ACTOR \
+        --env GITHUB_WORKFLOW \
+        --env GITHUB_HEAD_REF \
+        --env GITHUB_BASE_REF \
+        --env GITHUB_EVENT_NAME \
+        --env GITHUB_WORKSPACE=/github/workspace \
+        --env GITHUB_ACTION \
+        --env GITHUB_EVENT_PATH \
+        --env RUNNER_OS \
+        --env RUNNER_TOOL_CACHE \
+        --env RUNNER_TEMP \
+        --env RUNNER_WORKSPACE \
+        ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
         ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
         --volume "${githubHome}":"/root:z" \
         --volume "${githubWorkflow}":"/github/workflow:z" \
         --volume "${workspace}":"/github/workspace:z" \
-        --volume "${actionFolder}/steps":"/steps:z" \
-        --volume "${actionFolder}/entrypoint.sh":"/entrypoint.sh:z" \
-        ${sshAgent ? `--volume ${sshAgent}:/ssh-agent` : ''} \
-        ${sshAgent ? '--volume /home/runner/.ssh/known_hosts:/root/.ssh/known_hosts:ro' : ''} \
+        --volume "${actionFolder}/default-build-script:/UnityBuilderAction:z" \
+        --volume "${actionFolder}/platforms/ubuntu/steps:/steps:z" \
+        --volume "${actionFolder}/platforms/ubuntu/entrypoint.sh:/entrypoint.sh:z" \
         ${useHostNetwork ? '--net=host' : ''} \
         ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
         ${image} \
         /bin/bash /entrypoint.sh`;
-            yield (0, exec_1.exec)(command, undefined, { silent });
-        });
     },
 };
 exports["default"] = Docker;
--- src/model/docker.ts
+++ src/model/docker.ts
@@ -4,6 +4,17 @@ import path from 'path';
 
 const Docker = {
   async run(image, parameters, silent = false) {
+    let runCommand = '';
+    switch (process.platform) {
+      case 'linux':
+        runCommand = this.getLinuxCommand(image, parameters);
+        break;
+      case 'win32':
+        runCommand = this.getWindowsCommand(image, parameters);
+    }
+    await exec(runCommand, undefined, { silent });
+  },
+  getWindowsCommand(image, parameters): string {
     const {
       actionFolder,
       editorVersion,
@@ -28,7 +39,7 @@ const Docker = {
       testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]
     ).join(';');
 
-    const command = `docker run \
+    return `docker run \
         --workdir /github/workspace \
         --rm \
         --env UNITY_LICENSE \
@@ -43,6 +54,7 @@ const Docker = {
         --env COVERAGE_OPTIONS="${coverageOptions}" \
         --env COVERAGE_RESULTS_PATH="CodeCoverage" \
         --env ARTIFACTS_PATH="${artifactsPath}" \
+        --env GITHUB_WORKSPACE=c:/github/workspace \
         --env GITHUB_REF \
         --env GITHUB_SHA \
         --env GITHUB_REPOSITORY \
@@ -58,21 +70,90 @@ const Docker = {
         --env RUNNER_TOOL_CACHE \
         --env RUNNER_TEMP \
         --env RUNNER_WORKSPACE \
-        --env GIT_PRIVATE_TOKEN="${gitPrivateToken}" \
+        ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
+        ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
+        --volume "${workspace}":"c:/github/workspace" \
+        --volume "c:/regkeys":"c:/regkeys" \
+        --volume "C:/Program Files (x86)/Microsoft Visual Studio":"C:/Program Files (x86)/Microsoft Visual Studio" \
+        --volume "C:/Program Files (x86)/Windows Kits":"C:/Program Files (x86)/Windows Kits" \
+        --volume "C:/ProgramData/Microsoft/VisualStudio":"C:/ProgramData/Microsoft/VisualStudio" \
+        --volume "${actionFolder}/default-build-script":"c:/UnityBuilderAction" \
+        --volume "${actionFolder}/platforms/windows":"c:/steps" \
+        --volume "${actionFolder}/BlankProject":"c:/BlankProject" \
+        ${useHostNetwork ? '--net=host' : ''} \
+        ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
+        ${image} \
+        powershell c:/'steps/entrypoint.ps1`;
+  },
+
+  getLinuxCommand(image, parameters): string {
+    const {
+      actionFolder,
+      editorVersion,
+      workspace,
+      projectPath,
+      customParameters,
+      testMode,
+      coverageOptions,
+      artifactsPath,
+      useHostNetwork,
+      sshAgent,
+      gitPrivateToken,
+      githubToken,
+      runnerTemporaryPath,
+    } = parameters;
+
+    const githubHome = path.join(runnerTemporaryPath, '_github_home');
+    if (!existsSync(githubHome)) mkdirSync(githubHome);
+    const githubWorkflow = path.join(runnerTemporaryPath, '_github_workflow');
+    if (!existsSync(githubWorkflow)) mkdirSync(githubWorkflow);
+    const testPlatforms = (
+      testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]
+    ).join(';');
+
+    return `docker run \
+        --workdir /github/workspace \
+        --rm \
+        --env UNITY_LICENSE \
+        --env UNITY_LICENSE_FILE \
+        --env UNITY_EMAIL \
+        --env UNITY_PASSWORD \
+        --env UNITY_SERIAL \
+        --env UNITY_VERSION="${editorVersion}" \
+        --env PROJECT_PATH="${projectPath}" \
+        --env CUSTOM_PARAMETERS="${customParameters}" \
+        --env TEST_PLATFORMS="${testPlatforms}" \
+        --env COVERAGE_OPTIONS="${coverageOptions}" \
+        --env COVERAGE_RESULTS_PATH="CodeCoverage" \
+        --env ARTIFACTS_PATH="${artifactsPath}" \
+        --env GITHUB_WORKSPACE=/github/workspace \
+        --env GITHUB_REF \
+        --env GITHUB_SHA \
+        --env GITHUB_REPOSITORY \
+        --env GITHUB_ACTOR \
+        --env GITHUB_WORKFLOW \
+        --env GITHUB_HEAD_REF \
+        --env GITHUB_BASE_REF \
+        --env GITHUB_EVENT_NAME \
+        --env GITHUB_WORKSPACE=/github/workspace \
+        --env GITHUB_ACTION \
+        --env GITHUB_EVENT_PATH \
+        --env RUNNER_OS \
+        --env RUNNER_TOOL_CACHE \
+        --env RUNNER_TEMP \
+        --env RUNNER_WORKSPACE \
+        ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
         ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
         --volume "${githubHome}":"/root:z" \
         --volume "${githubWorkflow}":"/github/workflow:z" \
         --volume "${workspace}":"/github/workspace:z" \
-        --volume "${actionFolder}/steps":"/steps:z" \
-        --volume "${actionFolder}/entrypoint.sh":"/entrypoint.sh:z" \
-        ${sshAgent ? `--volume ${sshAgent}:/ssh-agent` : ''} \
-        ${sshAgent ? '--volume /home/runner/.ssh/known_hosts:/root/.ssh/known_hosts:ro' : ''} \
+        --volume "${actionFolder}/default-build-script:/UnityBuilderAction:z" \
+        --volume "${actionFolder}/platforms/ubuntu/steps:/steps:z" \
+        --volume "${actionFolder}/platforms/ubuntu/entrypoint.sh:/entrypoint.sh:z" \
         ${useHostNetwork ? '--net=host' : ''} \
         ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
         ${image} \
         /bin/bash /entrypoint.sh`;
-
-    await exec(command, undefined, { silent });
   },
 };
 
